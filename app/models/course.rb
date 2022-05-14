85a03fadb155b40986c6bdf1dac08846bb2b390e
Bondok6 <kirolousbondok6@gmail.com>
2022-05-14 18:54:22 +0200

    Add rich-text editor

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
   validates :title, :presence => true
   validates :description, :presence => true, :length => { :minimum => 5 }
 
+  has_rich_text :description
 end
